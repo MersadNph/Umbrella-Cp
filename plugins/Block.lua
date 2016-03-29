588ebd597d815faf4c19725eb306f6655de1b3e1
ArashEmp <ArashEmp@users.noreply.github.com>
2016-03-29 15:40:49 -0700

    Create Block.lua

---
+++ plugins/Block.lua
@@ -0,0 +1,24 @@
+local function run(msg,matches)
+if matches[1] == "+" and matches[2] then
+block_user("user#id"..matches[2], ok_cb, false)
+return "User "..matches[2].." has been Blocked"
+end
+
+if matches[1] == "-" and matches[2] then
+unblock_user("user#id"..matches[2], ok_cb, false)
+  return "User "..matches[2].." has been Unblocked"
+end
+end
+
+return {
+description = "Block And Unblock User From Pm"
+usage = {
+    "block + {user_id} : Block User From Private",
+    "block - {user_id} : Unblock User From Private",
+    },
+    patterns = {
+    "^[Bb]lock (+) (%d+)$",
+    "^[Bb]lock (-) (%d+)$",
+    },
+    run = run
+    }
